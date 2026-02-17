25b238e7f8a76dfd6f8ff04412b5587bff2b79db
Greyberet <58717769+Greyberet@users.noreply.github.com>
2026-02-17 13:32:18 -0500

    Initial config

---
+++ src/main/java/com/ministerio/starparking/StarParkingCoreApplication.java
@@ -0,0 +1,13 @@
+package com.ministerio.starparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarParkingCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarParkingCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ministerio/starparking/StarParkingCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ministerio.starparking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarParkingCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
